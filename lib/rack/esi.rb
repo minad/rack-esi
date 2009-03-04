cc16c4e617a4c17ced3b2d65cd9225ddf9c80246
Daniel Mendler <minad@slizer.hadiko.de>
2009-03-04 15:59:47 +0100

    code style

--- lib/rack/esi.rb
+++ lib/rack/esi.rb
@@ -1,54 +1,54 @@
-require 'rack'
-
 # See http://www.w3.org/TR/edge-arch
 # http://www.w3.org/TR/esi-lang
-class Rack::ESI
-  def initialize(app)
-    @app = app
-  end
-
-  def call(env)
-    env.delete 'HTTP_IF_NONE_MATCH'
-    env.delete 'HTTP_IF_MODIFIED_SINCE'
-
-    status, header, body = response = @app.call(env)
-    return response if !xml?(header)
-
-    body = join_body(body)
-
-    return response unless body.include?('<esi:')
-
-    body = process_esi(body, env)
-
-    header = header.merge('Content-Length' => body.size.to_s)
-    [status, header, [body]]
-  end
-
-  private
-
-  def process_esi(body, env)
-    body.gsub!(/<esi:remove>.*?<\/esi:remove>|<esi:comment[^>]*\/>|\s*xmlns:esi=("[^"]+"|'[^']+')/, '')
-    body.gsub!(/<esi:include\s+src=("[^"]+"|'[^']+')\s*\/>/) do
-      src = $1[1..-2]
-      uri = env['REQUEST_URI'] || env['PATH_INFO']
-      i = uri.index('?')
-      uri = src + (i ? uri[i..-1] : '')
-      inclusion_env = env.merge('PATH_INFO' => src,
-                                'REQUEST_PATH' => src,
-                                'REQUEST_URI' => uri)
-      join_body(@app.call(inclusion_env)[2]) # FIXME: Check the status
+module Rack
+  class ESI
+    def initialize(app)
+      @app = app
     end
-    body
-  end
 
-  def xml?(header)
-    # FIXME: Use another pattern
-    header['Content-Type'].to_s =~ /(ht|x)ml/
-  end
+    def call(env)
+      env.delete 'HTTP_IF_NONE_MATCH'
+      env.delete 'HTTP_IF_MODIFIED_SINCE'
 
-  def join_body(body)
-    parts = ''
-    body.each { |part| parts << part }
-    parts
+      status, header, body = response = @app.call(env)
+      return response if !xml?(header)
+
+      body = join_body(body)
+
+      return response unless body.include?('<esi:')
+
+      body = process_esi(body, env)
+
+      header['Content-Length'] = body.size.to_s
+      [status, header, [body]]
+    end
+
+    private
+
+    def process_esi(body, env)
+      body.gsub!(/<esi:remove>.*?<\/esi:remove>|<esi:comment[^>]*\/>|\s*xmlns:esi=("[^"]+"|'[^']+')/, '')
+      body.gsub!(/<esi:include\s+src=("[^"]+"|'[^']+')\s*\/>/) do
+        src = $1[1..-2]
+        uri = env['REQUEST_URI'] || env['PATH_INFO']
+        i = uri.index('?')
+        uri = src + (i ? uri[i..-1] : '')
+        inclusion_env = env.merge('PATH_INFO' => src,
+                                  'REQUEST_PATH' => src,
+                                  'REQUEST_URI' => uri)
+        join_body(@app.call(inclusion_env)[2]) # FIXME: Check the status
+      end
+      body
+    end
+
+    def xml?(header)
+      # FIXME: Use another pattern
+      header['Content-Type'].to_s =~ /(ht|x)ml/
+    end
+
+    def join_body(body)
+      parts = ''
+      body.each { |part| parts << part }
+      parts
+    end
   end
 end
